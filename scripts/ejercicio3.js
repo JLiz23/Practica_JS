1e3c1d25ac25bcab5d05b4292ee3df0ee6178786
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 01:05:28 -0600

    Se realizaron cambios de ejercicio3

---
+++ scripts/ejercicio3.js
@@ -0,0 +1,8 @@
+//Ejercicio 3//
+
+let puntajeJugador = 100;
+
+puntajeJugador += 50;
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>El nuevo puntuaje es: " + puntajeJugador + "</p>");
