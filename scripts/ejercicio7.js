97d028165b099fba998c17c3cbba8ee1139c7f65
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 11:59:57 -0600

    Se realizo el ejercicio7

---
+++ scripts/ejercicio7.js
@@ -0,0 +1,7 @@
+//ejercicio 7: Operador lógico AND (&&)
+let notaMatematicas = 8;
+let notaCiencias = 6;
+
+let ambasNotasAprobadas = (notaMatematicas >= 7 && notaCiencias >= 7);
+
+alert("¿Ambas notas son mayores o iguales a 7? " + ambasNotasAprobadas);
