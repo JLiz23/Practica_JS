40771a970d0098d67d0132e4c0062ad231334871
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 12:17:14 -0600

    Se realizo el ejercicio9

---
+++ scripts/ejercicio9.js
@@ -0,0 +1,8 @@
+let temperaturaActual = 35;
+
+document.write("<h2>Resultado</h2>");
+if (temperaturaActual > 30) {
+    document.write("<p>Hace calor</p>");
+} else {
+    document.write("<p>Clima agradable</p>");
+}
