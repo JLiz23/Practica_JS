ca5fa97973b6a67d98c398ce094cbef380392a5c
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 11:44:54 -0600

    Se realizo el ejercicio 6 y modifique el ejercicio 5

--- scripts/ejercicio5.js
+++ scripts/ejercicio5.js
@@ -2,6 +2,5 @@ let listarFrutas = ["Manzana", "Plátano"];
 
 listarFrutas.push("Naranja");
 
-document.write("<h2>Resultado:</h2>");
-document.write("<p>Arreglo actualizado: " + listaFrutas + "</p>");
-document.write("<p>La longitud total del arreglo es: " + listaFrutas.length + "</p>");
+console.log("Arreglo actualizado: " + listarFrutas);
+console.log("La longitud total del arreglo es: " + listarFrutas.length);
---
+++ scripts/ejercicio6.js
@@ -0,0 +1,5 @@
+ let numeroDiez = 10;
+ let textoDiez = "10";
+
+ console.log("igualdad debil (10 == \"10\"): "+ (numeroDiez == textoDiez)); 
+ console.log("igualdad estricta (10 === \"10\"): "+ (numeroDiez === textoDiez)); 
