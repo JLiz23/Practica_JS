03c4c755c9485c4423fe69c615145fabce3877db
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 12:08:38 -0600

    Se realizo ejercicio8

---
+++ scripts/ejercicio8.js
@@ -0,0 +1,6 @@
+let notaMatematicas = 8;
+let notaCiencias = 6;
+
+let algunaNotaAprobada = (notaMatematicas >= 7 || notaCiencias >= 7);
+
+console.log("¿Al menos una nota es aprobatoria? " + algunaNotaAprobada);
