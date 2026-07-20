5d02a32119361c4106949146f34988f402eec186
JLiz23 <jencyfranco682@gmail.com>
2026-07-19 23:55:21 -0600

    Se hizo ejercicio1 y se arreglo parte del index y css

---
+++ scripts/ejercicio1.js
@@ -0,0 +1,9 @@
+// Ejercicio 1//
+
+let base = 10;
+let altura = 5;
+
+let area = base * altura;
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>El área del rectángulo es: " + area + "</p>");
