5f24e98b4e425dbbb75e13f33e9907b044b5bca6
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 10:55:04 -0600

    Se modifico ejercicio3

---
+++ scripts/ejercicio4.js
@@ -0,0 +1,7 @@
+let saludoInicial = "Hola, ";
+let nombre = "Carlos";
+
+let mensajeCompleto = SaludoInicial + nombreUsuario;
+
+document.write("<h2>Resultado:</h2>");
+document.write("<p>" + mensajeCompleto + "</p>");
