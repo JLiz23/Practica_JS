42ee8b1a4db63559a78bb6678f44cc0b338a5181
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 01:02:14 -0600

    Se realizo ejercicio 3

---
+++ scripts/ejercicio2.js
@@ -0,0 +1,9 @@
+//ejercicio2//
+
+let valor1 = 15;
+let valor2 = 4;
+
+let residuo = valor1 % valor2;
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>El residuo de 15 % 4 es: " + residuo + "</p>");
