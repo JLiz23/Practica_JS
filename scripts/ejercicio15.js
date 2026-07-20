a77b0a06cbae91fde373bd7bc2b3095fb9567d4d
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 15:39:04 -0600

    se realizo el ejercicio 15

---
+++ scripts/ejercicio15.js
@@ -0,0 +1,6 @@
+let listarColores = ["Rojo", "Azul", "Verde"];
+
+document.write("<h2>Resultado</h2>");
+for (const colorActual of listarColores) {
+    document.write("<p>" + colorActual + "</p>");
+}
