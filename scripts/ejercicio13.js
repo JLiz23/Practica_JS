ddb2c2e392255e7a2b5717e85ff3d3c4887ba62c
JLiz23 <jencyfranco682@gmail.com>
2026-07-20 14:49:35 -0600

    Se realizaron los ejercicios 13

---
+++ scripts/ejercicio13.js
@@ -0,0 +1,7 @@
+let numeroPar = 2;
+
+document.write("<h2>Resultado</h2>");
+while (numeroPar <= 10) {
+    document.write("<p>" + numeroPar + "</p>");
+    numeroPar += 2; // Incrementar por 2 para obtener los números pares
+}
